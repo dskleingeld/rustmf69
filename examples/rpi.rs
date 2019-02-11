ad1aed51acf6e84a3aad683ec4071f2b8ceeb618
David Kleingeld <dskleingeld@gmail.com>
2019-02-12 00:56:08 +0100

    implements read and write reg funct and sync part of init

--- examples/rpi.rs
+++ examples/rpi.rs
@@ -4,18 +4,25 @@ extern crate pi_rmf69;
 
 use hal::spidev::SpidevOptions;
 use hal::Spidev;
+use hal::Pin;
+use hal::sysfs_gpio::Direction;
 use hal::Delay;
 
 use pi_rmf69::{Radio, FreqencyBand, Bitrate};
 
 fn main() {
-    let mut spi = Spidev::open("/dev/spidev0.0").unwrap();
+    let cs = Pin::new(8);
+    cs.export().unwrap();
+    cs.set_direction(Direction::Out).unwrap();
+
+    let mut spi = Spidev::open("/dev/spidev0.1").unwrap();
     let options = SpidevOptions::new()
         .max_speed_hz(500_000)
         .mode(hal::spidev::SPI_MODE_0)
         .build();
 		spi.configure(&options).unwrap();
 		
-		let mut radio = Radio::new(spi, Delay).unwrap();
-		radio.init(FreqencyBand::ISM433mhz, 0, 0, Bitrate::Standard);
+		let mut radio = Radio::new(spi, cs, Delay).unwrap();
+		radio.init(FreqencyBand::ISM433mhz, 0, 0, Bitrate::Standard)
+		     .expect("Could not init radio");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
-//working off example: https://github.com/japaric/mfrc522/blob/master/src/lib.rs
-#![no_std]
+//working off example (spi): https://github.com/japaric/mfrc522/blob/master/src/lib.rs
+//another example: https://github.com/JohnDoneth/hd44780-driver/blob/master/examples/raspberrypi/src/main.rs
+//#![no_std] //FIXME TODO remove all std lib dependencies
+mod registers;
+use registers::Register;
 
 extern crate embedded_hal as hal;
 use hal::blocking::spi;
 use hal::digital::OutputPin;
 use hal::spi::{Mode, Phase, Polarity};
-use hal::blocking::delay::DelayMs;
+use hal::blocking::delay::{DelayMs, DelayUs};
 
-pub struct Radio<SPI, D> {
+pub struct Radio<SPI, CS, D> {
 	spi: SPI,
+	cs: CS,
 	delay: D,
 	mode: RadioMode,
 	power_level: u8, //TODO restrict max-min
@@ -37,23 +41,80 @@ pub enum FreqencyBand {
 	ISM915mhz = 91,
 }
 
-impl<E, D, SPI> Radio<SPI, D> 
+impl<SPI,CS, D, E> Radio<SPI, CS, D>
 where SPI: spi::Transfer<u8, Error = E> + spi::Write<u8, Error = E>,
-      D: DelayMs<u16> {
+      D: DelayMs<u16>+DelayUs<u16>,
+      CS: OutputPin,
+      E : std::fmt::Debug {
 
-  pub fn new(spi: SPI, delay: D) ->	Result<Self, ()> {
-    Ok(Radio { spi, delay, mode: RadioMode::Rx, power_level: 31 })
+  pub fn new(spi: SPI, cs: CS, delay: D) ->	Result<Self, ()> {
+    Ok(Radio { spi, cs, delay, mode: RadioMode::Rx, power_level: 31 })
   }
 
-	pub fn init(&mut self, freq_band: FreqencyBand, node_id: u8, network_id: u8, speed: Bitrate) {
+	pub fn init(&mut self, freq_band: FreqencyBand, node_id: u8, network_id: u8, speed: Bitrate) -> Result<(),()> {
+
+		self.cs.set_high();
+
+		//check if the radio responds by seeing if we can change a register
+		let mut synced = false;
+		for _attempt in 0..100 {
+			self.write_reg(Register::Syncvalue1, 0xAA); //170
+			self.delay.delay_ms(1);
+			if self.read_reg(Register::Syncvalue1) == 0xAA {
+				synced = true;
+				break;
+			}
+		}
+		if !synced {return Err(())}
+
+		synced =	false;
+		for _attempt in 0..100 {
+			self.write_reg(Register::Syncvalue1, 0x55); //85
+			self.delay.delay_ms(1);
+			if self.read_reg(Register::Syncvalue1) == 0x55 {
+				synced = true;
+				break;
+			}
+		}
+		if !synced {return Err(())}
 		
-	
+
+
+		Ok(())
 	}
+
+
 
 
 	// pub fn send(uint8_t toAddress, const void* buffer, uint8_t bufferSize, bool requestACK=false) {
 	
 	
 	// }
+
+	fn write_reg(&mut self, addr: Register, value: u8) {
+		let to_write: [u8; 2] = [addr.write_address(), value];
+		self.cs.set_low();
+		self.spi.write(&to_write).unwrap();
+		self.cs.set_high();
+		self.delay.delay_us(15u16);
+		dbg!(to_write);
+
+	}
+
+	fn read_reg(&mut self, addr: Register) -> u8{
+		let mut to_transfer: [u8; 2] = [addr.read_address(), 0];
+		dbg!(to_transfer);
+
+		self.cs.set_low();
+
+		let to_transfer = self.spi.transfer(&mut to_transfer).unwrap();
+		self.cs.set_high();
+		self.delay.delay_us(15u16);
+
+		dbg!(to_transfer);
+
+		let awnser = to_transfer[1];
+		awnser
+	}
 
 }
